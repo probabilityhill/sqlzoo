66d18dbcfe15ec0bf8f8db83a419fb55b4af417b
okadanao <74280232+probabilityhill@users.noreply.github.com>
2023-04-17 20:38:07 +0900

    Update 01.sql

--- 02_select_from_world/01.sql
+++ 02_select_from_world/01.sql
@@ -1,2 +1,2 @@
--- Introduction
-SELECT name, continent, population FROM world
+-- Observe the result of running this SQL command to show the name, continent and population of all countries.
+SELECT name, continent, population FROM world;
