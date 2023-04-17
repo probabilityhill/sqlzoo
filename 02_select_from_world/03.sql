8b56b29f80d00dd6c74eb4a46890c35b7b372ef6
okadanao <74280232+probabilityhill@users.noreply.github.com>
2023-04-17 20:38:44 +0900

    Update 03.sql

--- 02_select_from_world/03.sql
+++ 02_select_from_world/03.sql
@@ -1,4 +1,4 @@
--- Per capita GDP
+-- Give the name and the per capita GDP for those countries with a population of at least 200 million.
 SELECT name, gdp / population AS 'per capita GDP'
 FROM world
 WHERE population >= 200000000;
